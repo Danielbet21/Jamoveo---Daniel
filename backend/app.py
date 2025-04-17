3e5bf76b782c025ce7446f4cd4d0233f2a2f0a3a
Daniel <danielbetzalel16@gmail.com>
2025-04-17 15:30:13 +0300

    Vercel adjustments v7

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-from app import create_app, socketio
+if __name__ == "__main__":
+    from app import create_app, socketio
+    import os
 
-app = create_app()
-
-
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+    app = create_app()
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
