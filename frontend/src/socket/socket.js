6c4d37e4ead864ba777da97e09960aa6bfad81d3
Daniel <danielbetzalel16@gmail.com>
2025-04-17 14:33:38 +0300

    Vercel adjustments v2

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://jamoveo-daniel-production.up.railway.app');
+
 export default socket;
 
 //This file is responsible for creating a socket connection to the server from the frontend.
